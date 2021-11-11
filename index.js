a9d81bbbf5e61ce84a26a52ccb3ceba7ede0b65d
l-goede <luise.goede@yahoo.de>
2021-11-11 18:18:19 +0100

    iteration 1-6

--- index.js
+++ index.js
@@ -1,27 +1,67 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    let newRecipe = {
+      title: "Curry Rice",
+      level: "Easy Peasy",
+      cuisine: "Indian",
+    };
+    return Recipe.create(newRecipe);
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then((newRecipe) => {
+    console.log(newRecipe.title);
+  })
+
+  .then(() => {
+    return Recipe.insertMany(data);
+  })
+  .then((data) => {
+    console.log(data);
+  })
+
+  .then(() => {
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 },
+      { new: true }
+    );
+  })
+  .then((update) => {
+    console.log("Successfully updated", update);
+  })
+
+  .then(() => {
+    return Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+  .then((deleted) => {
+    console.log("Successfully deleted", deleted);
+  })
+  .then(() => {
+    mongoose.disconnect();
+  })
+  .then(() => {
+    console.log("db disconnected");
+  })
+
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
